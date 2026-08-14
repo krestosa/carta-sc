2e9728241a1a68ac4ff045cf2cec2976acf7fcd4
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-14 15:05:08 -0300

    bust cache for subcategory navigation

--- _js_dev/main.js
+++ _js_dev/main.js
@@ -309,7 +309,7 @@ function noFoto(){
 	if (window.__scCatalogOverrideRequested) return;
 	window.__scCatalogOverrideRequested = true;
 
-	var assetVersion = '20260814-1501-spacing';
+	var assetVersion = '20260814-1503-subcategory';
 	window.__scCatalogAssetVersion = assetVersion;
 	function asset(path) { return path + '?v=' + assetVersion; }
 
@@ -362,7 +362,7 @@ function noFoto(){
 	if (window.__scUxMotionRequested) return;
 	window.__scUxMotionRequested = true;
 
-	var version = window.__scCatalogAssetVersion || '20260814-1501-spacing';
+	var version = window.__scCatalogAssetVersion || '20260814-1503-subcategory';
 	var script = document.createElement('script');
 	script.src = 'motion/motion.js?v=' + version;
 	script.async = true;
