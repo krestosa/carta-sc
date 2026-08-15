8b9919a30c8266ac5e301b65adb60bcbdb3d2037
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-15 02:02:33 -0300

    Fold mobile catalog spacing into layout

--- _js_dev/main.js
+++ _js_dev/main.js
@@ -2,7 +2,7 @@
 'use strict';
 if(window.__scLegacyMainLoaderBooted)return;window.__scLegacyMainLoaderBooted=true;
 
-var version='20260815-override-modules-v11';
+var version='20260815-override-modules-v12';
 window.__scCatalogAssetVersion=version;
 
 var root=document.documentElement;
