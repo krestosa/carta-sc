a8b70231c85661937c4452f795146efe59faa5cf
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-15 01:51:30 -0300

    Split category rail styles by viewport and controls

--- _js_dev/main.js
+++ _js_dev/main.js
@@ -2,7 +2,7 @@
 'use strict';
 if(window.__scLegacyMainLoaderBooted)return;window.__scLegacyMainLoaderBooted=true;
 
-var version='20260815-override-modules-v7';
+var version='20260815-override-modules-v8';
 window.__scCatalogAssetVersion=version;
 
 var root=document.documentElement;
