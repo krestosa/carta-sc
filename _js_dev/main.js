05dfc5b43251135e07d7c6a99c6827a941c159ab
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-15 02:21:26 -0300

    Stamp deployed asset versions from commit SHA

--- _js_dev/main.js
+++ _js_dev/main.js
@@ -2,7 +2,7 @@
 'use strict';
 if(window.__scLegacyMainLoaderBooted)return;window.__scLegacyMainLoaderBooted=true;
 
-var version='20260815-override-modules-v22';
+var version='unversioned';
 window.__scCatalogAssetVersion=version;
 
 var root=document.documentElement;
