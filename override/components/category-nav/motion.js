03ee8bd18198d7a9ef287234ba058a0805646468
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-17 20:08:28 -0300

    Delete unused empty category motion module

--- override/components/category-nav/motion.js
+++
@@ -1,6 +0,0 @@
-(function(){
-'use strict';
-var SC=window.SCOverride,N=SC&&SC.categoryNav;
-if(!SC||!N||SC.__categoryNavMotionBooted)return;SC.__categoryNavMotionBooted=true;
-N.installMotion=function(){};
-})();
