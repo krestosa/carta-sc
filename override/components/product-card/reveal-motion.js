5a7cc8d416f14e561bcdec2015a8a57be9d70ba1
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-19 08:15:22 -0300

    Elimina adapter de reveal obsoleto

--- override/components/product-card/reveal-motion.js
+++
@@ -1,7 +0,0 @@
-/* Las tarjetas estáticas no se ocultan ni reciben una entrada decorativa. */
-(function(){
-'use strict';
-var SC=window.SCOverride;if(!SC||SC.__productCardRevealMotionBooted)return;SC.__productCardRevealMotionBooted=true;
-var parts=SC.productCardMotionParts=SC.productCardMotionParts||{};
-parts.setupReveal=function(){parts.revealViewport=null;return function(){};};
-})();
