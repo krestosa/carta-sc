356766537e43fe03ef95fa6e5305f5074e8c685b
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-19 08:16:53 -0300

    Elimina adapter de carrito obsoleto

--- override/components/cart/list-motion.js
+++
@@ -1,7 +0,0 @@
-(function(){
-'use strict';
-/* Las filas del carrito no reciben una entrada decorativa por defecto. */
-var SC=window.SCOverride;if(!SC||SC.__cartListMotionBooted)return;SC.__cartListMotionBooted=true;
-var parts=SC.cartParts=SC.cartParts||{};
-parts.setupList=function(){return function(){};};
-})();
