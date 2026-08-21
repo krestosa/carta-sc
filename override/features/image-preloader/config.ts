eddd28226819cdfea83da90740c27ca2abe4f5cf
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-21 16:35:32 -0300

    reduce image loading pressure

--- override/features/image-preloader/config.ts
+++ override/features/image-preloader/config.ts
@@ -1,19 +1,19 @@
 export const imagePreloaderPolicy = Object.freeze({
-  loadAllImagesInBatches: true,
-  cacheImages: true,
+  loadAllImagesInBatches: false,
+  cacheImages: false,
 });
 
 export const imageBatchConfig = Object.freeze({
-  sync: 6,
-  size: 6,
-  budgetMs: 3,
-  idleTimeout: 900,
-  delayMs: 40,
+  sync: 2,
+  size: 4,
+  budgetMs: 2,
+  idleTimeout: 1200,
+  delayMs: 80,
 });
 
 export const MOBILE_LOGO_URL = 'https://www.sushiclub.com.ar/gfx/web-sushiclub2_black_m2.png';
 export const IMAGE_STAGE_SELECTOR = '.imgShop,.imgLiquidNoFillShop';
-export const NEAR_VIEWPORT_MARGIN = 160;
+export const NEAR_VIEWPORT_MARGIN = 0;
 
 export function criticalImageLimit(): number {
   const mode = document.documentElement.getAttribute('data-sc-catalog-view') ?? 'compact';
