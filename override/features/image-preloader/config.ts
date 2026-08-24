56058e6a2edd2665361d642884ed11933d0ba5eb
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-24 00:40:55 -0300

    Source preloader reveal delay from DTCG token

--- override/features/image-preloader/config.ts
+++ override/features/image-preloader/config.ts
@@ -1,3 +1,5 @@
+import { tokenRuntime } from '../../core/tokens.generated.js';
+
 export const imagePreloaderPolicy = Object.freeze({
   loadAllImagesInBatches: false,
   cacheImages: false,
@@ -8,7 +10,7 @@ export const imageBatchConfig = Object.freeze({
   size: 4,
   budgetMs: 2,
   idleTimeout: 1200,
-  delayMs: 80,
+  delayMs: tokenRuntime.durationMs['component.duration.imagePreloaderDelay'],
 });
 
 export const MOBILE_LOGO_URL = 'https://www.sushiclub.com.ar/gfx/web-sushiclub2_black_m2.png';
