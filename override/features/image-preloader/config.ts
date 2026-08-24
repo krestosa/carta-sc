82b6ed06dc2b211846d442880751dde28da89c3e
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-24 10:08:33 -0300

    Remove obsolete image batch config

--- override/features/image-preloader/config.ts
+++ override/features/image-preloader/config.ts
@@ -3,15 +3,6 @@ export const imagePreloaderPolicy = Object.freeze({
   cacheImages: false,
 });
 
-export const imageBatchConfig = Object.freeze({
-  sync: 2,
-  size: 4,
-  budgetMs: 2,
-  idleTimeout: 1200,
-  delayMs: 80,
-});
-
 export const MOBILE_LOGO_URL = 'https://www.sushiclub.com.ar/gfx/web-sushiclub2_black_m2.png';
 export const IMAGE_STAGE_SELECTOR = '.imgShop,.imgLiquidNoFillShop';
-export const NEAR_VIEWPORT_MARGIN = 0;
 export const IMAGE_WAVE_ROW_TOLERANCE_PX = 4;
